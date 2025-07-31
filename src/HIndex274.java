0fa0fda4a36a126ccf263b7925ef33cb9f04ccdc
RituLog <ritu.k.gupta67@gmail.com>
2025-07-31 14:38:29 +0530

    H- index 274

---
+++ src/HIndex274.java
@@ -0,0 +1,28 @@
+public class HIndex274 {
+    public static void main(String[] args) {
+       int[] citations = {3,0,6,1,5};
+       System.out.println(hIndex(citations));
+    }
+
+    static int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] arr = new int[n+1];
+
+        for(int c: citations){
+            if(c>n){
+                arr[n]++;
+            }
+            else{
+                arr[c]++;
+            }
+        }
+        int count =0;
+        for(int i=n; i>=0; i--){
+            count = count + arr[i];
+            if(count >= i){
+                return i;
+            }
+        }
+        return 0;
+    }
+}
